0ca2b4ba5074f637ab84d9c4b21f04fb637a2f54
Roony11-Repocitorioduoc <rica,sanchezs@duocuc.cl>
2024-04-10 13:17:42 -0400

    arreglo ciclo while

--- Python/menu_cmd.py
+++ Python/menu_cmd.py
@@ -3,27 +3,33 @@
 import os
 import time
 
-print(f'''
-Menu Principal
-1. ipconfig
-2. hola mundo
-
-''')
-
 while True:
+    os.system("cls")
+
+    print(f'''
+    Menu Principal
+    1. ipconfig
+    2. hola mundo
+    3. salir
+
+    ''')
+
     cPrincipal=str(input("\nIngresa tu opcion\n"))
 
-    if ((cPrincipal=="1") or (cPrincipal=="2")):
+    if ((cPrincipal=="1") or (cPrincipal=="2") or (cPrincipal=="3")):
         if (cPrincipal=="1"):
             os.system("cls")
 
             os.system("ipconfig")
             time.sleep(5)
-            break
-        if (cPrincipal=="2"):
+        elif (cPrincipal=="2"):
             print("Hola mundo!")
 
+            time.sleep(5)
+        elif (cPrincipal=="3"):
+            print("Saliendo...")
             time.sleep(5)
             break
     else:
         print("Ingresa una opcion valida!")
+        time.sleep(3)
