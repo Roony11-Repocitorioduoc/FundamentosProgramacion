354342ffa97b92b2eedc0db16039d6d17e50c520
Roony11 <rica.sanchezs@duocuc.cl>
2024-06-07 09:30:54 -0400

    hola

--- Python/proyectos/app_json-csv/maincsv.py
+++ Python/proyectos/app_json-csv/maincsv.py
@@ -7,7 +7,7 @@ lista_carac = []
 lista_dict = []
 
 def imc(peso, estatura, lista_imc):
-    lista_imc.append(peso/estatura**2)
+    lista_imc.append(round(peso/estatura**2, 1))
     
 def caracterizacion(imc):
     categorizacion = ""
@@ -66,4 +66,4 @@ with open("usuarios_ficticios.csv", "r", encoding="utf-8") as archivo:
 with open("usuarios_ficticios.json", "w", encoding="utf-8") as archivo:
     # Escribimos los diccionarios en un json
     for i in range(len(lista_dict)):
-        json.dump(lista_dict[i], archivo, indent=4)
+        json.dump(lista_dict, archivo, indent=4)
