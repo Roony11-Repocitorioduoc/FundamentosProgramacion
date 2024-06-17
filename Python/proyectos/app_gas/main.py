0af3498cb5cab5969101768aaa6bd14768127c5e
Roony11 <rica.sanchezs@duocuc.cl>
2024-06-17 10:01:50 -0400

    chupalo victor

---
+++ Python/proyectos/app_gas/main.py
@@ -0,0 +1,46 @@
+import validar
+import imprimir
+import os
+import modulo_venta_gas
+
+menu_principal = ["Crear BD de Ventas", "Lista Ventas", "Crear Reporte", "Estadisticas", "(En Contruccion)"]
+
+lista_gas = [] # Guarda dict de los gases generados
+
+# Menu principal
+control = 0
+
+# Strings
+ingreso_opc = f"Ingresa la opción del menú\n"
+
+while True:
+    imprimir.menu(menu_principal, f"Menu principal", True)
+    
+    control = validar.entero(ingreso_opc)
+    
+    if control == 1:
+        print(menu_principal[0])
+        lista_gas = modulo_venta_gas.cargar_data_test()
+        os.system("pause")
+    elif control == 2:
+        print(menu_principal[1])
+        modulo_venta_gas.listar_ventas_list(lista_gas)
+        os.system("pause")
+    elif control == 3:
+        print(menu_principal[2])
+        
+        os.system("pause")
+    elif control == 4:
+        print(menu_principal[3])
+        
+        os.system("pause")
+    elif control == 5:
+        print(menu_principal[4])
+        
+        os.system("pause")
+    elif control == 6:
+        break
+    else:
+        print(f"Ingresa una opción valída!")
+        
+        os.system("pause")
---
+++ Python/proyectos/app_gas/modulo_venta_gas.py
@@ -0,0 +1,55 @@
+import os
+import csv
+import random
+
+meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+def generar_valores_aleatorios(minimo, maximo):
+    """genera un valor aleatorios en un rango
+    """
+    aleatorio = random.randrange(minimo, maximo)
+    return aleatorio
+
+def cargar_data_test():
+    """Genera los datos para la BD en los 12 meses
+    """
+    
+    lista_dict = []
+    
+    for k in range(len(meses)):
+        valor = generar_valores_aleatorios(20000, 28000)
+        cantidad = generar_valores_aleatorios(200, 300)
+        
+        dict = {
+            "mes": meses[k],
+            "valor_gas": valor,
+            "cant_vendida": cantidad,
+            "total_mes": valor*cantidad
+        }
+        
+        lista_dict.append(dict)
+    
+    print("Datos cargados correctamente")
+    return lista_dict
+
+def listar_ventas_list(lista):
+    """ Muestra los datos de una lista de json con las variables estadisticas del mes
+    """
+    if len(lista)==0:
+        os.system("cls")
+        print(f"No hay datos que mostrar")
+        os.system("pause")
+        return
+    
+    # Recorremos el diccionario
+    os.system("cls")
+    for k in range(len(lista)):
+        # lista[k] es un diccionario
+        print(f"""MES : {lista[k]["mes"]}
+VALOR GAS : ${lista[k]["valor_gas"]}
+CANTIDAD VENDIDA : {lista[k]["cant_vendida"]}
+TOTAL MES : ${lista[k]["total_mes"]}
+""")
+    
+    # creara una tabla que printeara todo
+    return
